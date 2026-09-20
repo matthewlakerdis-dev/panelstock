c9ef4e2528daa1a584fffe31b6c5c5de98b8afc8
matthewlakerdis-dev <matthewlakerdis@gmail.com>
2026-09-21 07:29:52 +1000

    Harden legacy iPad client detection

--- site/app-loader.js
+++ site/app-loader.js
@@ -1,9 +1,10 @@
 (function(){
   'use strict';
   var ua=navigator.userAgent||'';
-  var legacy=/OS (?:10|9|8|7|6|5)_/.test(ua)&&/iP(?:ad|hone|od)/.test(ua);
+  var legacy=/iP(?:ad|hone|od)/.test(ua)&&(/OS (?:10|9|8|7|6|5)_/.test(ua)||!/Version\/(?:1[1-9]|[2-9][0-9])/.test(ua));
   var script=document.createElement('script');
-  script.src=legacy?'app.legacy.js?v=1':'app.js?v=security-2';
+  script.src=legacy?'app.legacy.js?v=2':'app.js?v=security-2';
   script.async=false;
+  script.onerror=function(){var el=document.getElementById('app');if(el)el.innerHTML='<div class="loading">PanelStock client failed to load.</div>';};
   document.body.appendChild(script);
 })();
